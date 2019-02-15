21f5f62534e363387c7dc59b4cf578b0d6253353
ryujunghyun <rjh1996@hanmail.net>
2019-02-15 13:14:44 +0900

    bell=1로 변경되는 코드

--- app/src/main/java/junghyun/myapplication/busLine.java
+++ app/src/main/java/junghyun/myapplication/busLine.java
@@ -1,11 +1,14 @@
 package junghyun.myapplication;
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -13,19 +16,26 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class busLine extends AppCompatActivity {
 
@@ -34,15 +44,17 @@ public class busLine extends AppCompatActivity {
     private static final String TAG_ID = "id";
     private static final String TAG_BNAME = "busname";
     private static final String TAG_SNAME = "bustopname";
+    private static final String TAG_BELL = "bell";
     private static final String TAG_ROAD = "road";
 
     ArrayList<HashMap<String, String>> mBusList;
+    ListAdapter adapter;
     ListView list;
     String myJSON;
     TextView textview;
     EditText editText;
-
-
+    Cursor c;
+    String clickstop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -69,8 +81,6 @@ public class busLine extends AppCompatActivity {
 
                 GetData searchBusLine = new GetData();
                 searchBusLine.execute(editText.getText().toString());
-
-
 
             }
         });
@@ -132,7 +142,6 @@ public class busLine extends AppCompatActivity {
                     "Please Wait", null, true, true);
         }
 
-
         @Override
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
@@ -155,15 +164,168 @@ public class busLine extends AppCompatActivity {
         protected String doInBackground(String... params) {
 
             String searchKeyword = params[0];
+            //String searchKeyword1 = params[1];
 
-            String serverURL = "http://192.168.0.9/bus.php";
+            String serverURL = "http://192.168.0.7/bus.php";
             String postParameters = "busname=" + searchKeyword;
-
+          //  String postParameters1 = "bustop=" + searchKeyword1;
+          //  int postParameters2 = "id=" +
             try {
 
                 URL url = new URL(serverURL);
                 HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
 
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+
+                bufferedReader.close();
+
+
+                return sb.toString().trim();
+
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+                errorString = e.toString();
+
+                return null;
+            }
+        }
+    }
+
+
+    private void showResult(){
+        try {
+            final JSONObject jsonObject = new JSONObject(myJSON);
+           final JSONArray busArray = jsonObject.getJSONArray(TAG_RESULT);
+            HashMap<String,String> BusHashMap;
+
+            for(int i=0;i<busArray.length();i++){
+
+                JSONObject item = busArray.getJSONObject(i);
+
+                String id = item.getString(TAG_ID);
+                String busname = item.getString(TAG_BNAME);
+                String bustopname = item.getString(TAG_SNAME);
+
+                BusHashMap = new HashMap<>();
+
+                BusHashMap.put(TAG_ID, id);
+                BusHashMap.put(TAG_BNAME, busname);
+                BusHashMap.put(TAG_SNAME, bustopname);
+
+                mBusList.add(BusHashMap);
+
+            }
+            ImageView road = (ImageView)findViewById(R.id.road);
+/*ListAdapter*/
+            adapter = new SimpleAdapter(
+                    busLine.this, mBusList, R.layout.list_item,
+                    new String[]{TAG_ID, TAG_BNAME,TAG_SNAME},
+                    new int[]{R.id.id,R.id.busname, R.id.bustopname}
+            );
+
+            list.setAdapter(adapter);
+            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {//int position, long id는 클릭된 항목의 id
+                    //1. 클릭된 mBusList리스트의 id나 location값으로 클릭된 정류장이름 찾아서  2.php로 보냄
+                    try {
+                        clickstop = busArray.getJSONObject(position).getString(TAG_SNAME);//클릭된 아이디로 정류장이름 찾기
+                        Toast.makeText(
+                                getApplicationContext(),
+                                "클릭된 정류장: "+clickstop,
+                                Toast.LENGTH_SHORT
+                        ).show();
+                        GetBustop searchBustop = new GetBustop();
+                        searchBustop.execute(clickstop);
+                    }
+                    catch (JSONException e) {
+                        Log.d(TAG, "showResult : ", e);
+                    }
+
+                }
+            });
+            list.setChoiceMode(ListView.CHOICE_MODE_SINGLE);//리스트 하나만 선택하도록 하는거*/
+        } catch (JSONException e) {
+
+            Log.d(TAG, "showResult : ", e);
+        }
+
+    }
+
+    private class GetBustop extends AsyncTask<String, Void, String>{
+
+        ProgressDialog progressDialog;
+        String errorString = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(busLine.this,
+                    "Please Wait", null, true, true);
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+            // textview.setText(result);
+            Log.d(TAG, "response - " + result);
+                clickstop=result;
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String searchKeyword = params[0];
+
+            String serverURL = "http://192.168.0.7/bus.php";
+            String postParameters = "clickstop=" + searchKeyword;
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
 
                 httpURLConnection.setReadTimeout(5000);
                 httpURLConnection.setConnectTimeout(5000);
@@ -220,44 +382,7 @@ public class busLine extends AppCompatActivity {
     }
 
 
-    private void showResult(){
-        try {
-            JSONObject jsonObject = new JSONObject(myJSON);
-            JSONArray busArray = jsonObject.getJSONArray(TAG_RESULT);
 
-            for(int i=0;i<busArray.length();i++){
-
-                JSONObject item = busArray.getJSONObject(i);
-
-                String id = item.getString(TAG_ID);
-                String busname = item.getString(TAG_BNAME);
-                String bustopname = item.getString(TAG_SNAME);
-
-                HashMap<String,String> BusHashMap = new HashMap<>();
-
-                BusHashMap.put(TAG_ID, id);
-                BusHashMap.put(TAG_BNAME, busname);
-                BusHashMap.put(TAG_SNAME, bustopname);
-
-                mBusList.add(BusHashMap);
-
-            }
-            ImageView road = (ImageView)findViewById(R.id.road);
-
-            ListAdapter adapter = new SimpleAdapter(
-                    busLine.this, mBusList, R.layout.list_item,
-                    new String[]{TAG_ID, TAG_BNAME,TAG_SNAME},
-                    new int[]{R.id.id,R.id.busname, R.id.bustopname}
-            );
-
-            list.setAdapter(adapter);
-
-        } catch (JSONException e) {
-
-            Log.d(TAG, "showResult : ", e);
-        }
-
-    }
 /*
     public void getData(String url) {
         class GetDataJSON extends AsyncTask<String, Void, String> {
